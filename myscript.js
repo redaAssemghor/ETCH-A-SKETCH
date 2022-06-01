a982879bfb2df11d6d76fde3960f045773bbd151
yamakuza <rednaxreda@gmail.com>
2022-06-01 19:00:46 +0100

    hover effect form js

--- myscript.js
+++ myscript.js
@@ -1,13 +1,31 @@
 const container = document.getElementById("container");
 
+
+
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    cell.innerText = (i + 1);
+    
     container.appendChild(cell).className = "grid-item";
   };
 };
 
 makeRows(16, 16);
+
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(event) {
+  event.target.classList.replace("grid-item", "color");
+});
+
+
+const btn = document.querySelector("button");
+btn.addEventListener('click', function() {
+  let a = prompt ("Enter number of squares per side:" , 16);
+  if (a != null && a < 100) {
+    return makeRows(a,a);
+  }
+});
+
